c01430aa7c9ea905e88a4dac45b76f9cd57aa316
Brian Duong <brianminhduong@gmail.com>
2022-06-25 15:57:44 -0400

    Add remove isLoggedIn from local storage with logoutHandler

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    const storedUserLoggedInInformation = localStorage.getItem("isLoggedin");
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
 
     if (storedUserLoggedInInformation === "1") {
       setIsLoggedIn(true);
@@ -23,6 +23,7 @@ function App() {
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
